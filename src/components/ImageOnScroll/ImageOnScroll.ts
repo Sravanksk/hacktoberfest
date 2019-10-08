493637dbb414bad03ef98903b538b890aa4c149c
Kai Wißler <kai.wissler@schwarzfisch.virtual-identity.net>
2019-10-08 17:15:13 +0200

    fix: use other name for scrolltop

--- src/components/ImageOnScroll/ImageOnScroll.ts
+++ src/components/ImageOnScroll/ImageOnScroll.ts
@@ -41,7 +41,7 @@ class ImageOnScroll extends Component< ImageOnScrollProps, ImageOnScrollState > 
     private sizeX = 0;
     private sizeY = 0;
     private docSizeY = 0;
-    public scrollTop = 0;
+    private scrollY = 0;
     private scrollBottom = 0;
 
     static attributes = [
@@ -72,29 +72,29 @@ class ImageOnScroll extends Component< ImageOnScrollProps, ImageOnScrollState > 
         this.accordion.style.left = (this.sizeX - this.openWidth)/2 + "px";
         this.accordion.style.width = this.openWidth + "px";
         this.accordion.style.height = this.openHeight + "px";
-        if(this.scrollTop<this.headerHeight){
+        if(this.scrollY<this.headerHeight){
             console.log("phase1");
             this.accordion.style.position = "absolute";
             this.accordion.style.top = this.headerHeight + "px";
             this.accordion.style.left = (this.sizeX - this.closedWidth)/2 + "px";
             this.accordion.style.width = this.closedWidth + "px";
             this.accordion.style.height = this.closedHeight + "px";
-        }else if(this.scrollTop<this.headerHeight+this.stretchWidth){
+        }else if(this.scrollY<this.headerHeight+this.stretchWidth){
             console.log("phase2");
-            currentWidth = this.closedWidth + (this.scrollTop-this.headerHeight);
+            currentWidth = this.closedWidth + (this.scrollY-this.headerHeight);
             this.accordion.style.position = "fixed";
             this.accordion.style.top = '0';
             this.accordion.style.left = (this.sizeX - currentWidth)/2 + "px";
             this.accordion.style.width = currentWidth + "px";
             this.accordion.style.height = this.closedHeight + "px";
-        }else if(this.scrollTop<(this.headerHeight+this.stretchWidth+this.openHeight-this.closedHeight)){
+        }else if(this.scrollY<(this.headerHeight+this.stretchWidth+this.openHeight-this.closedHeight)){
             console.log("phase3");
             currentWidth = this.openWidth;
             this.accordion.style.position = "fixed";
             this.accordion.style.top = '0';
             this.accordion.style.left = (this.sizeX - currentWidth)/2 + "px";
             this.accordion.style.width = currentWidth + "px";
-            this.accordion.style.height = (this.closedHeight + (this.scrollTop-(this.headerHeight+this.stretchWidth))) + "px";
+            this.accordion.style.height = (this.closedHeight + (this.scrollY-(this.headerHeight+this.stretchWidth))) + "px";
         }else{
             console.log("phase4");
             currentWidth = this.openWidth;
@@ -119,9 +119,9 @@ class ImageOnScroll extends Component< ImageOnScrollProps, ImageOnScrollState > 
         }
 
         if (scroll.changed) {
-            console.log("scroll");
-            console.log(scroll);
-            this.scrollTop = scroll.top;
+            //console.log("scroll");
+            //console.log(scroll);
+            this.scrollY = scroll.top;
             this.scrollBottom = scroll.bottom;
             this.viewportScroll();
         }
